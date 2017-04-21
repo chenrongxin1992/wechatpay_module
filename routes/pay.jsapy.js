158bc1e25c8701d8f81e2899e67de7a1468c4689
chenrongxin <848536190@qq.com>
2017-04-21 10:30:49 +0800

    0421_change_callback_url

--- routes/pay.jsapy.js
+++ routes/pay.jsapy.js
@@ -28,7 +28,7 @@ var path = {
 var device_info = 'WEB',
     fee_type = 'CNY',
     trade_type = weChatTools.payTypes.JSAPI, //交易类型
-    notify_url = 'http://test.api.pay.w-lans.cn/pay/notice'; //订单通知地址
+    notify_url = 'http://test.api.pay.178wifi.com/pay/notice'; //订单通知地址
 
 //下单
 exports.PlaceOrder = function (req, res, next) {
